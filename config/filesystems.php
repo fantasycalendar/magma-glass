473eead885f81e4a9cfa987010f937c4418d7a1a
V13Axel <alex@stechstudio.com>
2021-10-04 21:07:18 -0400

    filesystem config

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'articles' => [
+            'driver' => env('ARTICLE_DRIVER','local'),
+            'root' => env('ARTICLE_ROOT', storage_path('notes'))
+        ]
+
     ],
 
     /*
