0bd31a9fdf174f21ae6e1d00560569c6e0dac86c
V13Axel <alex@stechstudio.com>
2021-10-05 23:45:18 -0400

    Placeholder logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,8 +67,8 @@
                 </div>
 
                 <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                    <div class="flex-shrink-0 flex items-center px-4">
-                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                    <div class="flex-shrink-0 flex items-center px-4 text-center">
+                        <img class="h-18 mx-auto" src="{{ asset('images/logo.png') }}" alt="Workflow">
                     </div>
                     <nav class="mt-5 px-2 space-y-1">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
@@ -128,15 +128,9 @@
                 <div class="flex-shrink-0 flex bg-gray-700 p-4">
                     <a href="#" class="flex-shrink-0 group block">
                         <div class="flex items-center">
-                            <div>
-                                <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                            </div>
                             <div class="ml-3">
-                                <p class="text-base font-medium text-white">
-                                    Tom Cook
-                                </p>
-                                <p class="text-sm font-medium text-gray-400 group-hover:text-gray-300">
-                                    View profile
+                                <p class="text-sm font-medium text-gray-400">
+                                    {{ $page_name ?? config('app.name') }}
                                 </p>
                             </div>
                         </div>
@@ -155,8 +149,8 @@
                 <!-- Sidebar component, swap this element with another sidebar if you like -->
                 <div class="flex-1 flex flex-col min-h-0 bg-gray-700">
                     <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-                        <div class="flex items-center flex-shrink-0 px-4">
-                            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                        <div class="flex items-center flex-shrink-0 px-4 text-center">
+                            <img class="h-18 mx-auto" src="{{ asset('images/logo.png') }}" alt="Workflow">
                         </div>
                         <nav class="mt-5 flex-1 px-2 bg-gray-700 space-y-1">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
@@ -213,22 +207,8 @@
                             </a>
                         </nav>
                     </div>
-                    <div class="flex-shrink-0 flex bg-gray-700 p-4">
-                        <a href="#" class="flex-shrink-0 w-full group block">
-                            <div class="flex items-center">
-                                <div>
-                                    <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                                </div>
-                                <div class="ml-3">
-                                    <p class="text-sm font-medium text-white">
-                                        Tom Cook
-                                    </p>
-                                    <p class="text-xs font-medium text-gray-300 group-hover:text-gray-200">
-                                        View profile
-                                    </p>
-                                </div>
-                            </div>
-                        </a>
+                    <div class="flex-shrink-0 flex bg-gray-700 text-gray-400 p-4">
+                        {{ $page_name ?? config('app.name') }}
                     </div>
                 </div>
             </div>
