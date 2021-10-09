401f06ae8fa0523b4a5e3fb4229ecc1be50927d9
V13Axel <alex@stechstudio.com>
2021-10-09 15:16:51 -0400

    Remove dd

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,8 +10,6 @@ class ArticleController extends Controller
 {
     public function index($articlePath = '')
     {
-        dd(ArticleCache::populate()['links']);
-
         return view('show_article', [
             'isIndex' => $articlePath == '',
             'article' => RetrieveArticle::dispatchSync($articlePath)
