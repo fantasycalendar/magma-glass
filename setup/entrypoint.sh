978682e84709c0dc5da37f00db27d4f59a7a173b
V13Axel <alex@stechstudio.com>
2021-11-15 20:55:29 -0500

    Create deploy key during entrypoint

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 cd /app/ || exit 1;
 
-SSH_KEY=/app/storage/app/id_rsa
-if test -f "$SSH_KEY"; then
-    echo "$SSH_KEY exists."
+SSH_KEYFILE=/app/storage/app/id_rsa
+if test -f "$SSH_KEYFILE"; then
+    echo "$SSH_KEYFILE exists."
+    echo "Verifying file permissions..."
+
+    fuid="$(stat --format '%u' "$SSH_KEYFILE")"
+    cuid=$(id -u)
+    if [[ $cuid -eq 0 ]]; then
+        cuid=33
+    fi
+
+    echo "File is owned by ID $fuid"
+
+    if [ "${fuid}" = "${cuid}" ]; then
+        echo "File is owned by current user ${cuid}. Moving on."
+    else
+        echo "Key file exists, but is not owned by current user ${cuid}. Please fix that."
+        exit 1;
+    fi
 else
     echo "Generating Github Deployment key..."
-    ssh-keygen -N '' -t ed25519 -C "Magma Glass Key" -f /app/storage/app/id_rsa
+    ssh-keygen -N '' -t ed25519 -C "Magma Glass Key" -f "$SSH_KEYFILE"
+    chown www-data:www-data "$SSH_KEYFILE"
+    chown www-data:www-data "$SSH_KEYFILE.pub"
     echo "Github deployment key generated! Starting services."
 fi
 
