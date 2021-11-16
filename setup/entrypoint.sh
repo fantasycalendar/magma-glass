33d11cb26b89b68f985bfa137511ebbbba92f3fe
V13Axel <alex@stechstudio.com>
2021-11-15 19:46:46 -0500

    wip

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 cd /app/ || exit 1;
+
+SSH_KEY=/app/storage/app/id_rsa
+if test -f "$SSH_KEY"; then
+    echo "$SSH_KEY exists."
+fi
+
+
 service redis-server start
 php artisan optimize
 php artisan route:cache
