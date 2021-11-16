c6580101513bc8862d25eba003ce9cd4cc67c8aa
V13Axel <alex@stechstudio.com>
2021-11-15 20:19:11 -0500

    Setup for github deploy key

--- config/magmaglass.php
+++ config/magmaglass.php
@@ -7,4 +7,6 @@ return [
     'github_repo' => env('GITHUB_REPO', null),
     'github_deploy_key' => env('GITHUB_DEPLOY_KEY', null),
     'github_repo_subdir' => env('GITHUB_REPO_SUBDIR', ''),
+
+    'github_deploy_key_path' => env('GITHUB_DEPLOY_KEY_PATH', '/app/storage/app/id_rsa')
 ];
--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -4,6 +4,10 @@ cd /app/ || exit 1;
 SSH_KEY=/app/storage/app/id_rsa
 if test -f "$SSH_KEY"; then
     echo "$SSH_KEY exists."
+else
+    echo "Generating Github Deployment key..."
+    ssh-keygen -N '' -t ed25519 -C "Magma Glass Key" -f /app/storage/app/id_rsa
+    echo "Github deployment key generated! Starting services."
 fi
 
 
